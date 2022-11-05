9ab53e63b7356a5bf3909cf4d063dbc919d47f07
Olesya <olesya.sadygova@list.ru>
2022-11-05 21:45:22 +0300

    Написала код в файл Exampl008_Задача_

--- Exampl008_Задача_/Program.cs
+++ Exampl008_Задача_/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = -9;
+int numberB = -3;
+
+int max = numberA;
+int min =numberB;
+
+if(numberA > numberB) max = numberA; min = numberB;
+if(numberA < numberB) max = numberB; min = numberA;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
+
+
+
