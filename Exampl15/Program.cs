301983bd923410bb5c7ba536687f8abdc1b0bca9
Olesya <olesya.sadygova@list.ru>
2022-11-13 19:06:49 +0300

    Добавила новую папку

---
+++ Exampl15/Program.cs
@@ -0,0 +1,28 @@
+﻿void FIllArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index = index+1;
+
+    }
+}
+
+void PrintArray(int[] col);
+{
+    int count = col.Lenght;
+    int position = 0;
+    While (position < count);
+    {
+        Console.WriteLine(col[position]);
+        position++;
+
+}
+}
+
+int[] array = new int[10];
+
+FIllArray(array);
+PrintArray(array);
