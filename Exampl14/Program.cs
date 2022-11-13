f598c96c3da02c6a2ce5f2d964e33fbce66ed0f0
Olesya <olesya.sadygova@list.ru>
2022-11-13 19:06:10 +0300

    Добавила новую папку

---
+++ Exampl14/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1,2,3,34,87,54,6,79,87,9};
+
+int n = array.Length;
+int find = 87;
+
+int index = 0;
+while (index < n )
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+  index = index+1;  
+}
+
