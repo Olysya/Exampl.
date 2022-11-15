112d1c2083ba7743b7a48d52de5686d9b8e17235
Olesya <olesya.sadygova@list.ru>
2022-11-15 22:54:37 +0300

    Добавила новую папку

---
+++ Exampl19/Program.cs
@@ -0,0 +1,19 @@
+﻿int x = 3;
+
+if (x ==1)
+Console.WriteLine("Рабочий день");
+else if (x==2)
+Console.WriteLine("Рабочий день");
+else if (x==3)
+Console.WriteLine("Рабочий день");
+else if (x==4)
+Console.WriteLine("Рабочий день");
+else if (x==5)
+Console.WriteLine("Рабочий день");
+else if (x==6)
+Console.WriteLine("Ура, выходной!");
+else if (x==7)
+Console.WriteLine("Ура,выходной!");
+
+
+
