62c33a7448655b50eb01668cbd3dc4831a2da77c
Olesya <olesya.sadygova@list.ru>
2022-11-18 12:16:02 +0300

    Добавила новую папку

--- Exampl18/Program.cs
+++ Exampl18/Program.cs
@@ -1,5 +1,11 @@
-﻿int x = 782;
-
-    x = x / 10 % 10;
-    Console.Write("Вторая цифра числа = ");
-    Console.WriteLine (x);
+﻿Console.Write ("Введите число, для которого нужно найти третью по счету цифру: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a >99)
+{
+    while (a>999)
+    {
+        a=a/10;
+    }
+  Console.Write ("Третья по счету цифра: {0}", a%10);
+  }
+else Console.Write ("Третьей  цифры нет");
