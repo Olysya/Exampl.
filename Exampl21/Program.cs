70ef5b6a247fbb4137c073f990bcb40499c9e299
Olesya <olesya.sadygova@list.ru>
2022-11-18 13:22:36 +0300

    Добавила новую папку

---
+++ Exampl21/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = 3;
+int i;
+
+for (int i = 1; i <= number; i++);
+{ 
+ Console.Write($"{i*i*i} ");
+}
+int ReadInt(string message)
+{
+ Console.Write(message);
+ return Convert.ToInt32(Console.ReadLine());
+}
