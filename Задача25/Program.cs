7ae2a424695801a3ce7e4261c313e06884945150
Olesya <olesya.sadygova@list.ru>
2022-11-22 18:42:17 +0300

    Добавила новую папку

---
+++ Задача25/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу которая на вход принимает два числа (А и В), и показывает число Ав степени В
+Console.WriteLine("введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
+
+for (int i = 1; i < b; i++)
+{
+step = step * a;
+}
+Console.WriteLine("A в степени B равно: " + step);
